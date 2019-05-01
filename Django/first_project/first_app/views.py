afd277ac7faa69b425e6cb278df953a9f0bfce09
Hannah Higgins <hannahiggins@hotmail.com>
2019-05-01 17:25:14 +0100

    Added form and validation

--- Django/first_project/first_app/views.py
+++ Django/first_project/first_app/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Topic, Webpage, AccessRecord
+from . import forms
 
 
 # Create your views here.
 def index(request):
-    my_dict = {'insert_me': 'Hello I am from views.py!'}
-    return render(request, 'first_app/index.html', context=my_dict)
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records': webpages_list}
+    return render(request, 'first_app/index.html', context=date_dict)
+
+
+def form_name_view(request):
+    form = forms.FormName()
+    if request.method == "POST":
+        form = forms.FormName(request.POST)
+        if form.is_valid():
+            print("VALIDATION SUCCESS")
+            print(form.cleaned_data['name'])
+            print(form.cleaned_data['email'])
+            print(form.cleaned_data['text'])
+    return render(request, 'first_app/form_page.html', {'form': form})
